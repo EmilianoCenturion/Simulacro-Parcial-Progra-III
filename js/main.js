d7c2648f61abec5fad277dd259ff2f4f0ec9bcb4
EmilianoCenturion <emilianodavidcenturion@gmail.com>
2025-09-29 23:37:12 -0300

    Modificados todos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const librosTienda = [
-    {id:1, nombre:"alicia en el pais de las maravillas", precio: 150, ruta_img: "https://imgs.search.brave.com/ZLVczFBH1yNSnL-_yP5zkCu7wRDtDS-Z3VmAT7p24tg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzY1NDQzNi1NTEE3/NTIyNDE1NzQzNV8w/MzIwMjQtVi53ZWJw"},
-    {id:2, nombre:"moby dick", precio: 350, ruta_img: "https://imgs.search.brave.com/jIEQw2oGsnfX_iiImOSAa9EYhueuR7vbQw4HCw_CMXQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzk1NjI1MC1NTEE1/MzYxMzcxMjgyNV8w/MjIwMjMtVi53ZWJw"},
-    {id:3, nombre:"caperucita roja", precio: 250, ruta_img: "https://imgs.search.brave.com/VOkLNKd2xCuor5CVDISOTaUNyFT6U1iJSVR0Agc-a-I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzc0NDcyOC1NTFU3/ODk3MTU2Nzc4OF8w/OTIwMjQtVi53ZWJw"},
-    {id:4, nombre:"las viudas de los jueves", precio: 50, ruta_img: "https://imgs.search.brave.com/O_4jd4_XejpgDG9R2mQ-9mSFD_PNIXovVaMtH3ESJ98/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzgxQnJNN1crTmZT/LmpwZw"},
-    {id:5, nombre:"donde esta wally", precio: 500, ruta_img: "https://imgs.search.brave.com/2-aozIkAbZcynyV6BPZnv5JmtaqvVg64rYCbZWWzZD0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzg5MTE4OC1NTFU3/MjU5MTQ4MjE2NV8x/MDIwMjMtVi53ZWJw"},
-    {id:6, nombre:"el señor de los anillos", precio: 980, ruta_img: "https://imgs.search.brave.com/CPwl2Vs-nbmNobq4hCAVQZp4UpUf2A1KXr6EN6FCH64/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzY4ODE4OC1NTE01/MjM1MTMzMjM1OF8x/MTIwMjItVi53ZWJw"} 
+    {id:1, nombre:"arandano", precio: 150, ruta_img: "img/arandano.jpg"},
+    {id:2, nombre:"banana", precio: 350, ruta_img: "img/banana.jpg"},
+    {id:3, nombre:"frambuesa", precio: 250, ruta_img: "img/frambuesa.png"},
+    {id:4, nombre:"frutilla", precio: 50, ruta_img: "img/frutilla.jpg"},
+    {id:5, nombre:"kiwi", precio: 500, ruta_img: "img/kiwi.jpg"},
+    {id:6, nombre:"mandarina", precio: 980, ruta_img: "img/mandarina.jpg"}, 
+    {id:7, nombre:"manzana", precio: 980, ruta_img: "img/manzana.jpg"}, 
+    {id:8, nombre:"naranja", precio: 980, ruta_img: "img/naranja.jpg"}, 
+    {id:9, nombre:"pera", precio: 980, ruta_img: "img/pera.jpg"}, 
+    {id:10, nombre:"anana", precio: 980, ruta_img: "img/anana.jpg"}, 
+    {id:11, nombre:"pomelo-amarillo", precio: 980, ruta_img: "img/pomelo-amarillo.jpg"}, 
+    {id:12, nombre:"pomelo-rojo", precio: 980, ruta_img: "img/pomelo-rojo.jpg"}, 
+    {id:12, nombre:"sandia", precio: 980, ruta_img: "img/sandia.jpg"} 
 ];
 
 
@@ -53,33 +60,16 @@ const inputImg = document.getElementById("input-img");
 
 const BttnOrdenar = document.getElementById("bttn-ordenar")
 
+const carritoCantidad = document.getElementById("carrito-cantidad")
+
+const botonVaciar = document.getElementById("boton-vaciar")
 /* 
 Escuchadores de eventos 
 */
 
+botonVaciar.addEventListener("click", vaciarCarrito)
+
 barraBusqueda.addEventListener("keyup", filtrarProducto)
-
-formAgregar.addEventListener("submit", function(value){
-    value.preventDefault();
-
-    const nuevoLibro = {
-        id: librosTienda.length + 1,
-        nombre: inputNombre.value,
-        precio: parseInt(inputPrecio.value),
-        ruta_img: inputImg.value
-    }
-
-    librosTienda.push(nuevoLibro),
-
-    mostrarLista(librosTienda)
-
-    inputNombre.value = ""
-    inputPrecio.value = ""
-    inputImg.value = ""
-})
-
-BttnOrdenar.addEventListener("click", ordenarLibros
-)
 
 /*
 Funciones  
@@ -92,19 +82,21 @@ function mostrarLista(array){
         <div class="card-producto">
             <img class ="img-card" src="${libro.ruta_img}" alt="${libro.nombre}">
             <h3 class = "text-card">${libro.nombre}</h3>
-            <p >${libro.precio}$</p>
+            <p class= "">${libro.precio}$</p>
             <a class="bttn-card" onclick="agregarACarrito(${libro.id})">Agregar al carrito</a>
         </div>
         `
     })
+    
     contenedorProductos.innerHTML = htmlProductos;
 };
 
 function mostrarCarrito() {
-    let htmlCarrito = "<ul>";
+    let htmlCarrito = "<ul class=lista-carrito>";
     carrito.forEach(libroCarrito =>{
         htmlCarrito += 
         `<li class="bloque-item">
+            <img src="${libroCarrito.ruta_img}" alt="${libroCarrito.nombre}">
             <p class="nombre-item">${libroCarrito.nombre} - ${libroCarrito.precio}</p>
             <button onclick="eliminarDelCarrito(${libroCarrito.id})"class="boton-eliminar">Eliminar</button>
         </li>
@@ -115,6 +107,7 @@ function mostrarCarrito() {
     console.log(htmlCarrito);
 
     contenedorCarrito.innerHTML = htmlCarrito;
+    carritoCantidad.innerHTML = `Carrito: ${carrito.length}`
 }
 
 function filtrarProducto() {
@@ -147,9 +140,15 @@ function eliminarDelCarrito(libroCarritoId) {
     mostrarCarrito(carrito);
 }
 
+function vaciarCarrito() {
+    carrito.length = 0;
+    mostrarCarrito();
+}
+
 function agregarACarrito(idLibro) {
     carrito.push(librosTienda.find(libro => libro.id == idLibro))
     mostrarCarrito(carrito)
+
 }
 
 function init(){
